1cb996808562ec8710b4870ddcd4e657a66cdbfd
Waldemar Ortiz-Calo <wortizcalo@gmail.com>
2020-12-20 15:25:26 -0700

    Loading Data

---
+++ 2.R_Code/1.Scripts/PlottingScript.R
@@ -0,0 +1,25 @@
+#   Script Details                                                          ####
+
+# Author: Waldemar Ortiz-Calo
+
+# Date:2020-12-20 
+
+# Purpose: 
+
+###############################################################################
+#   Library / Functions / Data                                              ####
+
+##  Library                                                                 ####
+library(tidyverse)
+
+##  Functions                                                               ####
+
+##  Data                                                                    ####
+female_annual   <- read_csv("1.Data/Processed/annual_f.csv")
+female_seasonal <- read_csv("1.Data/Processed/seasonal_f.csv")
+female_monthly  <- read_csv("1.Data/Processed/monthly_f.csv")
+male_annual     <- read_csv("1.Data/Processed/annual_m.csv")
+male_seasonal   <- read_csv("1.Data/Processed/seasonal_m.csv")
+male_monthly    <- read_csv("1.Data/Processed/monthly_m.csv")
+ 
+###############################################################################
